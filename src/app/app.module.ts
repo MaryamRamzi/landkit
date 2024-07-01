0801e5457a38fee82ce1176c90a7cc94a393fc11
MaryamRamzi <93717996+MaryamRamzi@users.noreply.github.com>
2024-07-01 13:17:35 +0330

    create new component & add new path in navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,27 @@ import {NgxUsefulSwiperModule} from "ngx-useful-swiper";
 import { HomeComponent } from './main/home/home.component';
 import { LandingsComponent } from './main/landings/landings.component';
 import {Routes,RouterModule} from "@angular/router";
+import {AccountsComponent} from "./main/accounts/accounts.component";
+import {DocumentationComponent} from "./main/documentation/documentation.component";
+import {PagesComponent} from "./main/pages/pages.component";
 
 const routs : Routes=[
   { path:'', component:HomeComponent},
   { path:'home', component:HomeComponent},
   { path:'landings', component:LandingsComponent},
+  { path: 'accounts' , component:AccountsComponent},
+  {path: 'documentation', component:DocumentationComponent},
+  {path: 'pages', component:PagesComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LandingsComponent
+    LandingsComponent,
+    AccountsComponent,
+    DocumentationComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
+import { AccountComponent } from './Account/account/account.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { DocumantationComponent } from './documantation/documantation.component';
+import { DocumentationComponent } from './documentation/documentation.component';
+import { PagesComponent } from './pages/pages.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AccountComponent,
+    AccountsComponent,
+    DocumantationComponent,
+    DocumentationComponent,
+    PagesComponent
+  ],
   imports: [
     CommonModule,
     MainRoutingModule
